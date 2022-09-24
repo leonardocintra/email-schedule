789b74391f667e9393679efb60929b4054112b9c
Leonardo Cintra <leonardo.ncintra@outlook.com>
2022-09-24 17:04:51 -0300

    feat: create entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,26 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { SendgridModule } from './app/sendgrid/sendgrid.module';
+import { MailModule } from './app/mail/mail.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    SendgridModule],
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      database: process.env.DB_DATABASE,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      synchronize: true,
+      entities: [__dirname + '/**/*.entity{.js,.ts}']
+    }),
+    SendgridModule,
+    MailModule
+  ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/app/mail/mail.entity.ts
@@ -0,0 +1,25 @@
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity({ name: 'mails' })
+export class MailEntity {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({ name: 'destination_address', nullable: false })
+    destinationAddress: string;
+
+    @Column({ name: 'due_date', type: 'datetime', nullable: false })
+    dueDate: string;
+
+    @Column({ name: 'status' })
+    status: string;
+
+    @CreateDateColumn({ name: 'created_at' })
+    createdAt: string;
+
+    @UpdateDateColumn({ name: 'updated_at' })
+    updateddAt: string;
+
+    @DeleteDateColumn({ name: 'deleted_at'})
+    deletedAt: string;
+}
