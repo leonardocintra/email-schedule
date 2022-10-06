26e81e4695740876920fb87bd24d11fe79260ce1
Leonardo Cintra <leonardo.ncintra@outlook.com>
2022-10-06 18:42:40 -0300

    chore: remove bot

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './configs/TypeOrmModuleOptions';
 import { SendgridModule } from './app/sendgrid/sendgrid.module';
 import { MailModule } from './app/mail/mail.module';
-import { MailbotService } from './cronzinho/mailbot/mailbot.service';
 import { ScheduleModule } from '@nestjs/schedule';
-import { MailbotModule } from './cronzinho/mailbot/mailbot.module';
 @Module({
   imports: [
     ConfigModule.forRoot(),
@@ -14,7 +12,6 @@ import { MailbotModule } from './cronzinho/mailbot/mailbot.module';
     ScheduleModule.forRoot(),
     SendgridModule,
     MailModule,
-    MailbotModule,
   ],
   controllers: [],
   providers: [],
--- src/cronzinho/mailbot/mailbot.module.ts
+++
@@ -1,9 +0,0 @@
-import { HttpModule } from '@nestjs/axios';
-import { Module } from '@nestjs/common';
-import { MailbotService } from './mailbot.service';
-
-@Module({
-  imports: [HttpModule],
-  providers: [MailbotService],
-})
-export class MailbotModule {}
